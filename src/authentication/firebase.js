0a881c7bd357cd2f05eef1962ea1a5cd9b309c94
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-03-03 11:52:44 +0000

    Firebase installed,Firebase configuartion is set up , .env variable is also done

---
+++ src/authentication/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_6Rfr5aQAxwCGgZDKtZSYFNq4xuQqYY",
+  authDomain: "subscription-box-apps.firebaseapp.com",
+  projectId: "subscription-box-apps",
+  storageBucket: "subscription-box-apps.firebasestorage.app",
+  messagingSenderId: "1077849250193",
+  appId: "1:1077849250193:web:edacfab759bfe096dc6230"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
