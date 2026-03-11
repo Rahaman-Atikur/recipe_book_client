832331d151ab0364b90b71c310a6ba5b7abfdb57
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-03-11 01:11:28 +0000

    Firebase Authentication in progress, SignIn and SingUp components are ready to implement, Register or SingnUp is already done

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -1,76 +1,76 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-// import { signInUser } from './SignIn';
+// import React from 'react';
+// import Navbar from '../components/Navbar';
+// // import { signInUser } from './SignIn';
 
-const Login = () => {
+// const Login = () => {
 
-  // const handleLogin = async (e) => {
-  //   e.preventDefault();
+//   // const handleLogin = async (e) => {
+//   //   e.preventDefault();
 
-  //   const form = e.target;
+//   //   const form = e.target;
 
-  //   const email = form.email.value;
-  //   const password = form.password.value;
+//   //   const email = form.email.value;
+//   //   const password = form.password.value;
 
-  //   console.log(email, password);
+//   //   console.log(email, password);
 
-  //   const user = await signInUser(email, password);
+//   //   const user = await signInUser(email, password);
 
-  //   console.log(user);
-  // };
+//   //   console.log(user);
+//   // };
 
-  return (
-    <div>
-      <Navbar />
+//   return (
+//     <div>
+//       <Navbar />
 
-      <div className="relative w-full h-screen">
+//       <div className="relative w-full h-screen">
 
-        <img
-          src="/images/1_rms91PTLhpkL0Qrj6lZR_g.webp"
-          alt="background"
-          className="w-full h-full object-cover"
-        />
+//         <img
+//           src="/images/1_rms91PTLhpkL0Qrj6lZR_g.webp"
+//           alt="background"
+//           className="w-full h-full object-cover"
+//         />
 
-        <form  className="absolute inset-0 flex items-center justify-center bg-black/40">
+//         <form  className="absolute inset-0 flex items-center justify-center bg-black/40">
 
-          <div className="bg-white p-8 rounded-xl shadow-lg w-96">
+//           <div className="bg-white p-8 rounded-xl shadow-lg w-96">
 
-            <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+//             <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
-            <div className="mb-4">
-              <label className="block mb-1 font-medium">Email</label>
-              <input
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-                className="w-full px-3 py-2 border rounded-lg"
-              />
-            </div>
+//             <div className="mb-4">
+//               <label className="block mb-1 font-medium">Email</label>
+//               <input
+//                 name="email"
+//                 type="email"
+//                 placeholder="Enter your email"
+//                 className="w-full px-3 py-2 border rounded-lg"
+//               />
+//             </div>
 
-            <div className="mb-6">
-              <label className="block mb-1 font-medium">Password</label>
-              <input
-                name="password"
-                type="password"
-                placeholder="Enter your password"
-                className="w-full px-3 py-2 border rounded-lg"
-              />
-            </div>
+//             <div className="mb-6">
+//               <label className="block mb-1 font-medium">Password</label>
+//               <input
+//                 name="password"
+//                 type="password"
+//                 placeholder="Enter your password"
+//                 className="w-full px-3 py-2 border rounded-lg"
+//               />
+//             </div>
 
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
-            >
-              Login
-            </button>
+//             <button
+//               type="submit"
+//               className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
+//             >
+//               Login
+//             </button>
 
-          </div>
+//           </div>
 
-        </form>
+//         </form>
 
-      </div>
-    </div>
-  );
-};
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
--- src/authentication/Register.jsx
+++ src/authentication/Register.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase';
 // import { registerUser } from './registerFunction';
 
 const Register = () => {
-//     const handleRegister = async (e) => {
-//     e.preventDefault();
+    const handleRegister = async (e) => {
+        e.preventDefault();
 
-//     const form = e.target;
+        const form = e.target;
 
-//     const name = form.name.value;
-//     const email = form.email.value;
-//     const password = form.password.value;
+        //     const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
 
-//     console.log(name, email, password);
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("User registered:", userCredential.user);
+            alert("successfully registered");
 
-//     const user = await registerUser(email, password);
-//     console.log(user);
-// };
-
-
+        }
+        catch (error) {
+            console.error("Error registering user:", error);
+            alert("Failed to register user");
+        }
+    };
     return (
         <div className="relative w-full h-screen">
             <Navbar></Navbar>
@@ -31,11 +37,11 @@ const Register = () => {
             />
 
             {/* Overlay Form */}
-            <form  className="absolute inset-0 flex items-center justify-center bg-black/40">
+            <form onSubmit={() => handleRegister} className="absolute inset-0 flex items-center justify-center bg-black/40">
                 <div className="bg-white p-8 rounded-xl shadow-lg w-96">
                     <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
 
-                    <div className="mb-4">
+                    {/* <div className="mb-4">
                         <label className="block mb-1 font-medium">Name</label>
                         <input
                             name='name'
@@ -43,7 +49,7 @@ const Register = () => {
                             placeholder="Enter your name"
                             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
-                    </div>
+                    </div> */}
 
                     <div className="mb-4">
                         <label className="block mb-1 font-medium">Email</label>
